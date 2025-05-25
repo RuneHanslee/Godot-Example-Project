5865ae83e8fd67b71dfc910a8de8825242e8b33d
RuneHanslee <skovronj12@gmail.com>
2025-05-24 18:03:26 -0700

    asdf

---
+++ example-project/Scripts/gravity_field.gd
@@ -0,0 +1,45 @@
+extends NinePatchRect
+
+# Export 
+@export var speed := 900
+
+# Onready
+@onready var collision := $Area2D/CollisionShape2D
+@onready var player := %Player
+@onready var gravitysfx := $GravityInverter
+
+# Variables
+var frame := 0.0
+var in_player := false
+
+# Ready
+func _ready() -> void:
+	collision.scale = size - Vector2(32,32)
+	collision.position = collision.scale / 2 + Vector2(16,16)
+
+func _process(delta: float) -> void:
+	if in_player:
+		frame += delta * speed / 60
+	else:
+		frame += delta * speed / 200
+	if frame > 8:
+		frame = 0
+	if frame < 0:
+		frame = 8
+	region_rect.position.x = floor(frame)*48
+	if in_player:
+		var rot := rotation - deg_to_rad(90)
+		player.velocity += Vector2(cos(rot), sin(rot)) * speed * delta
+		player.remove_gravity = true
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	in_player = true
+	player.velocity /= 3
+	gravitysfx.play()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	in_player = false
+	#player.velocity *= 1.2
--- example-project/Scripts/player.gd
+++ example-project/Scripts/player.gd
@@ -3,38 +3,51 @@ class_name Player
 
 # Export Variables
 
-@export var walk_speed := 2200.0
+@export var walk_speed := 2000.0
 @export var friction := 0.94
 @export var jump_length := 0.1
-@export var jump_per_length := 1800.0
-@export var jump_height := 200.0
+@export var jump_per_length := 1500.0
+@export var jump_height := 180.0
 @export var gravity := 900.0
 
 # Onready Variables
 
+@onready var jumpsfx := $SFX/Jump
+@onready var floorsfx := $SFX/Floor
 @onready var sprite := $AnimatedSprite2D
 
 # Variables
 
+var remove_gravity := false
 var can_jump := false
 var x_input := 0
 var jump_left := 0.0
+var just_on_floor := false
+
+var squish_physics := Vector2(1,1)
+var squish_velocity := Vector2(0,0)
 
 # Functions
 func movement(delta: float):
 	velocity.x += x_input * walk_speed * delta # X Movement
 	velocity.x *= pow(friction / 100000, delta)
-	velocity.y += gravity * delta
+	sprite.rotation = lerp_angle(sprite.rotation,deg_to_rad(x_input*4),0.03)
+	if !remove_gravity:
+		velocity.y += gravity * delta
 	
 	if jump_left > 0.0 && Input.is_action_pressed("Jump") && !is_on_ceiling() && !is_on_floor(): # Increases Velocity if jump key is held
 		jump_left = max(jump_left - delta, 0.0)
 		velocity.y -= jump_per_length * delta
+		squish_velocity += Vector2(-0.05,0.05)
+		jumpsfx.play()
 	else:
-		jump_left = 0
+		jump_left = 0 
 	
 	if can_jump && Input.is_action_just_pressed("Jump"): # Initializes Jump
 		jump_left = jump_length
 		velocity.y -= jump_height
+	remove_gravity = false
+	
 
 func do_animations():
 	var anim = "Idle"
@@ -44,7 +57,19 @@ func do_animations():
 		anim = "Air"
 	if sprite.animation != anim:
 		sprite.play(anim)
+	if is_on_floor() && !just_on_floor:
+		if !just_on_floor:
+			squish_velocity += Vector2(0.3,-0.3)
+			floorsfx.play()
+		just_on_floor = true
+	if !is_on_floor():
+		just_on_floor = false
 
+func do_squish_physics(delta):
+	squish_velocity.x = lerpf(squish_velocity.x, (1-squish_physics.x) * 0.2, 0.3)
+	squish_velocity.y = lerpf(squish_velocity.y, (1-squish_physics.y) * 0.2, 0.3)
+	squish_physics+=squish_velocity
+	sprite.scale = squish_physics
 
 # Process
 
@@ -58,3 +83,4 @@ func _process(delta: float) -> void:
 	movement(delta)
 	move_and_slide()
 	do_animations()
+	do_squish_physics(delta)
